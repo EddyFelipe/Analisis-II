77a2054222e146f7e7daa051388863f9dbce8d1d
Felipe <eddigzol@gmail.com>
2019-07-07 16:57:02 -0600

    creacion de la rama develop

--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -16,7 +16,8 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("Hola mundo");
+        fgeneral fg = new fgeneral();
+        fg.setVisible(true);
     }
     
 }
